569c14562b25dbe5c438335067d832937b4c69b4
NithiyananthanHarilo <hari23may@gmail.com>
2022-06-07 17:55:46 +0530

    first commit

---
+++ function/results.js
@@ -0,0 +1,38 @@
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+
+exports.handler = async function (event, context) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+    headless: true,
+  });
+
+  await page.goto("https://spacejelly.dev/");
+
+  await page.focus("#search-query");
+  await page.keyboard.type("api");
+
+  const results = await page.$eval("#search-query + div a", (links) => {
+    return links.map((link) => {
+      return {
+        text: link.innertext,
+        href: link.href,
+      };
+    });
+  });
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: "Ok",
+      page: {
+        title,
+        description,
+      },
+    }),
+  };
+};
